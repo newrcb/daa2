971660c60eaee4b637c8cb4a7923ed37d7c1512e
newrcb <rowdybunny4@gmail.com>
2024-11-25 23:38:02 +0530

    Update warshall2.cpp

--- warshall2.cpp
+++ warshall2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-const int MAX = 100; // Maximum number of vertices
-
-// Function to compute the transitive closure using Warshall's algorithm
+const int MAX = 100;
 void warshall(int graph[MAX][MAX], int n) {
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
@@ -13,8 +11,6 @@ void warshall(int graph[MAX][MAX], int n) {
         }
     }
 }
-
-// Function to print the adjacency matrix
 void printMatrix(int graph[MAX][MAX], int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -27,21 +23,16 @@ void printMatrix(int graph[MAX][MAX], int n) {
 int main() {
     int n;
     int graph[MAX][MAX];
-
     cout << "Enter the number of vertices: ";
     cin >> n;
-
     cout << "Enter adjacency matrix:\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> graph[i][j];
         }
     }
-
     warshall(graph, n);
-
     cout << "Transitive Closure (Reachability Matrix):\n";
     printMatrix(graph, n);
-
     return 0;
 }
